51b5114707786232ecf6e7c37d3eb0242593bf3b
haruvathsan <57910805+haruvathsan@users.noreply.github.com>
2023-04-08 20:19:57 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style.css';
+import Banner from './Banner';
+export default function App() {
+  return (
+    <div>
+      <Banner/>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-dmv6c4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-dmv6c4)
